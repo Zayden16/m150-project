1b3db4a4dc277fbbb1eb88172302332ed8fe4956
Raphael Härtel <raphael.haertel@bluewin.ch>
2021-11-25 14:58:17 +0100

    Update demo data script

--- fiscus-backend/Database/DemoData.sql
+++ fiscus-backend/Database/DemoData.sql
@@ -1,54 +1,52 @@
 BEGIN;
 
-INSERT INTO "User"("User_IBAN", "User_ReferenceNumber", "User_FirstName", "User_LastName", "User_DisplayName", "User_Mail", "User_Password")  
-VALUES ('CH1504835157229200000', '21000000003139471430009017', 'Thomas', 'Shelby', 'Tom', 'tom@gmail.com', 'password');
+INSERT INTO "Group"("Name", "Description")  
+VALUES ('Peaky blinders', 'Birmingham gang');
 
-INSERT INTO "User"("User_IBAN", "User_ReferenceNumber", "User_FirstName", "User_LastName", "User_DisplayName", "User_Mail", "User_Password")  
-VALUES ('CH7208781000134597300', '53200001105000312897842002', 'Arthur', 'Shelby', 'Arthur', 'arthur@gmail.com', 'password');
+INSERT INTO "User"("Username", "Email", "Password", "GroupId")  
+VALUES ('thomas.shelby', 'thomas.shelby@gmail.com', 'password', 1);
 
-INSERT INTO "TaxRate"("Taxrate_Percentage", "Taxrate_Description")  
-VALUES (5, 'Tax');
+INSERT INTO "User"("Username", "Email", "Password", "GroupId")  
+VALUES ('arthur.shelby', 'arthur.shelby@gmail.com', 'password', 1);
 
-INSERT INTO "ArticleUnit"("ArticleUnit_Text")  
-VALUES ('stk');
-INSERT INTO "ArticleUnit"("ArticleUnit_Text")  
-VALUES ('kg');
-INSERT INTO "ArticleUnit"("ArticleUnit_Text")  
-VALUES ('g');
-INSERT INTO "ArticleUnit"("ArticleUnit_Text")  
-VALUES ('m');
-INSERT INTO "ArticleUnit"("ArticleUnit_Text")  
-VALUES ('m3');
-INSERT INTO "ArticleUnit"("ArticleUnit_Text")  
-VALUES ('cm');
-INSERT INTO "ArticleUnit"("ArticleUnit_Text")  
-VALUES ('cm3');
-INSERT INTO "ArticleUnit"("ArticleUnit_Text")  
-VALUES ('l');
-INSERT INTO "ArticleUnit"("ArticleUnit_Text")  
-VALUES ('dl');
-INSERT INTO "ArticleUnit"("ArticleUnit_Text")  
-VALUES ('cl');
+INSERT INTO "ShoppingList"("GroupId")  
+VALUES (1);
 
-INSERT INTO "Article"("Article_Title", "Article_Description", "Article_Price", "Article_TaxRate", "Article_Unit")  
-VALUES ('TestArticle', 'This is a test.', 15.99, 1, 1);
+INSERT INTO "Category"("Name")  
+VALUES ('Grocery');
+INSERT INTO "Category"("Name")  
+VALUES ('Bar/Party');
+INSERT INTO "Category"("Name")  
+VALUES ('Rent');
+INSERT INTO "Category"("Name")  
+VALUES ('Bill');
+INSERT INTO "Category"("Name")  
+VALUES ('Excursion/Culture');
+INSERT INTO "Category"("Name")  
+VALUES ('Health');
+INSERT INTO "Category"("Name")  
+VALUES ('Shopping');
+INSERT INTO "Category"("Name")  
+VALUES ('Restaurant');
+INSERT INTO "Category"("Name")  
+VALUES ('Accommodation');
+INSERT INTO "Category"("Name")  
+VALUES ('Transport');
+INSERT INTO "Category"("Name")  
+VALUES ('Sport');
+INSERT INTO "Category"("Name")  
+VALUES ('Reimbursement');
 
-INSERT INTO "DocumentType"("DocumentType_Description")  
-VALUES ('Invoice');
+INSERT INTO "Cost"("Price", "Description", "Date", "CategoryId", "GroupId")  
+VALUES (99, 'Einkauf', '10.10.2021', 1, 1);
 
-INSERT INTO "DocumentStatus"("DocumentStatus_Description")  
-VALUES ('Active');
+INSERT INTO "Payment"("PaymentId", "UserId", "IsPayee", "CostId")  
+VALUES (1, 1, true, 1);
 
-INSERT INTO "Plz"("Plz_Number", "Plz_City")  
-VALUES ('6330', 'Zug');
+INSERT INTO "Payment"("PaymentId", "UserId", "IsPayee", "CostId")  
+VALUES (1, 2, true, 1);
 
-INSERT INTO "Customer"("Customer_FirstName", "Customer_LastName", "Customer_Address1", "Customer_Address2", "Customer_Email", "Customer_PlzId")  
-VALUES ('James', 'Bond', 'Chamerstrasse 15', '', 'james@gmail.com', 1);
-
-INSERT INTO "Document"("Document_Number", "Document_TypeId", "Document_CreatorId", "Document_SendeeId", "Document_StatusId")  
-VALUES (100, 1, 1, 1, 1);
-
-INSERT INTO "ArticlePosition"("Article_Id", "Document_Id", "Article_Quantity")  
-VALUES (1, 1, 15);
+INSERT INTO "Item"("Name", "Quantity", "IsPurchased", "ShoppingListId", "CategoryId", "UserId")  
+VALUES ('Beer', 6, true, 1, 1, 1);
 
 END;
